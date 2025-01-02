9c29d4f60bcf146807e305b671d10c401617d4b8
NanoChip <NanoChip@DESKTOP-2104TCA>
2025-01-02 09:16:46 +0200

    struct

---
+++ Assignmoop01/Point.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignmoop01
+{
+    internal struct Point
+    {
+        #region Struct: point
+        //  public int X;
+        // public int Y;
+
+        // Constructor : Special Method
+        //              : Named like Strct
+        //              : Has No Return TYpe
+
+        //public Point ()
+        //{
+
+        //}
+
+        //public Point()
+        //{
+        //    X = 12;
+        //    Y = 12;
+
+        //  }
+        //public Point(int x, int y)
+        //{
+
+        //    X = x;
+        //    Y = y;
+        //}
+
+        //public void PrintPoint()
+        //{
+        //    Console.WriteLine( $"({X}, {Y})");
+        //}
+        //public override string ToString()
+        //{
+        //    return $"({X}, {Y})";
+
+        // }
+
+        #endregion
+
+    }
+}
--- Assignmoop01/Program.cs
+++ Assignmoop01/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Struct
+
+            // Struct : Value Type : Stack
+
+            // Employee 
+            // id , name , salary 
+
+
+            // Point 
+
+            //  Point Po1;
+            // Declare For Object from ' Type ' "Point"
+            // Po1 'Object '
+            // Allocte Un Intialize 8 Bytes At Stack
+
+
+            //Console.WriteLine(Po1.X);
+            //Console.WriteLine(Po1.Y);
+
+            //Po1.X = 12;
+            //Po1.Y = 12;
+
+            //Console.WriteLine(Po1.X);
+            //Console.WriteLine(Po1.Y);
+
+            //  Po1 = new Point(1, 2);
+            // 
+
+            //Console.WriteLine(Po1.X);
+            //Console.WriteLine(Po1.Y);
+
+            //Po1.PrintPoint();
+            // Console.WriteLine(Po1.ToString);
+
+            // Console.WriteLine(Po1.ToString());
+
+            #endregion
+
+
         }
     }
-}
+ }
