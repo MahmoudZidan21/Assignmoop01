a832f308cccc888c72b1b15d7c4c5d5bae5dae6a
NanoChip <NanoChip@DESKTOP-2104TCA>
2025-01-02 15:57:51 +0200

    Struct

---
+++ oopS01/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace oopS01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Struct
+
+            // Struct : Value Type : Stack
+
+            // Employee 
+            // id , name , salary 
+
+
+            // Point 
+
+            //  Point Po1;
+            // Declare For Object from ' Type ' "Point"
+            // Po1 'Object '
+            // Allocte Un Intialize 8 Bytes At Stack
+
+
+            //Console.WriteLine(Po1.X);
+            //Console.WriteLine(Po1.Y);
+
+            //Po1.X = 12;
+            //Po1.Y = 12;
+
+            //Console.WriteLine(Po1.X);
+            //Console.WriteLine(Po1.Y);
+
+            //  Po1 = new Point(1, 2);
+            // 
+
+            //Console.WriteLine(Po1.X);
+            //Console.WriteLine(Po1.Y);
+
+            //Po1.PrintPoint();
+            // Console.WriteLine(Po1.ToString);
+
+            // Console.WriteLine(Po1.ToString());
+
+            #endregion
+        }
+    }
+}
